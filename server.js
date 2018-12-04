22bcee5bf9e210bd8edcd927190adb05253f2dc5
Chris Gilbert <chrisgilbert4@icloud.com>
2018-12-04 15:51:46 +0000

    tests added for users routes

--- server.js
+++ server.js
@@ -5,14 +5,13 @@ const users = require('./routes/api/users');
 const app = express();
 
 //Bodyparser Middleware
-
 app.use(bodyParser.json());
 
 // DB Config
-const db = require('../makers_academy/JavaScript-Projects/Makers-Airbnb/config/keys').mongoURI;
+// const db = require('../makers_academy/JavaScript-Projects/Makers-Airbnb/config/keys').mongoURI;
 
 //Connect to Mongo
-mongoose.connect(db)
+mongoose.connect("mongodb://admin:makers2018@ds113136.mlab.com:13136/makers_bnb")
     .then(() => console.log('MongoDB connected...'))
     .catch(err => console.log(err));
 
@@ -22,3 +21,5 @@ app.use('/api/users', users);
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
+
+module.exports = app;
